2efb50251d584a23e07f347808853d29d59180b8
Danylo Moraes <16506747+DanyloMoraes@users.noreply.github.com>
2025-03-31 13:30:18 -0300

    criado o Adapter para popular spinner com nomes dos cursos

--- app/src/main/java/devandroid/danylo/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/danylo/applistacurso/view/MainActivity.java
@@ -3,8 +3,10 @@ package devandroid.danylo.applistacurso.view;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -18,7 +20,6 @@ import java.util.List;
 import devandroid.danylo.applistacurso.R;
 import devandroid.danylo.applistacurso.controller.CursoController;
 import devandroid.danylo.applistacurso.controller.PessoaController;
-import devandroid.danylo.applistacurso.model.Curso;
 import devandroid.danylo.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     PessoaController controller;
     CursoController cursoController;
     Pessoa pessoa;
-    List<Curso> listaDeCursos;
+    List<String> nomesDosCursos;
     EditText editPrimeiroNome;
     EditText editSobrenome;
     EditText editNomeCurso;
@@ -35,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     Button buttonLimpar;
     Button buttonSalvar;
     Button buttonFinalizar;
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
         controller = new PessoaController(MainActivity.this);
         controller.toString();
-
-        cursoController = new CursoController();
-        listaDeCursos = cursoController.getListaDeCursos();
 
         pessoa = new Pessoa();
         controller.buscar(pessoa);
@@ -69,6 +69,18 @@ public class MainActivity extends AppCompatActivity {
         editSobrenome.setText(pessoa.getSobreNome());
         editNomeCurso.setText(pessoa.getTelefoneContato());
         editTelefone.setText(pessoa.getTelefoneContato());
+
+        cursoController = new CursoController();
+        nomesDosCursos = cursoController.dadosParaSpinner();
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                cursoController.dadosParaSpinner());
+
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+
+        spinner.setAdapter(adapter);
 
         buttonLimpar.setOnClickListener(new View.OnClickListener() {
             @Override
